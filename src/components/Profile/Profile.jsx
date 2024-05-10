75c7c2102eb422bc6bdad89f7289d2ae8eee960e
ArturVayutskiy <vayutskiy@gmail.com>
2024-05-10 22:17:26 +0300

    bugfixing/request update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,27 @@
 import styles from "./Profile.module.css";
 
-const Profile = () => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardItem}>
-        <img className={styles.cardImg}
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-        <p className={styles.cardTitleName}>Petra Marica</p>
-        <p className={styles.cardCaption}>@pmarica</p>
-        <p className={styles.cardCaption}>Salvador, Brasil</p>
-      </div>   
+        <img className={styles.cardImg} src={image} alt={name} />
+        <p className={styles.cardTitleName}>{name}</p>
+        <p className={styles.cardCaption}>@{tag}</p>
+        <p className={styles.cardCaption}>{location}</p>
+      </div>
 
       <ul className={styles.cardList}>
         <li className={styles.cardListItem}>
           <span>Followers</span>
-          <span className={styles.cardListNumber}>1000</span>
+          <span className={styles.cardListNumber}>{stats.followers}</span>
         </li>
         <li className={styles.cardListItem}>
           <span>Views</span>
-          <span className={styles.cardListNumber}>2000</span>
+          <span className={styles.cardListNumber}>{stats.views}</span>
         </li>
         <li className={styles.cardListItem}>
           <span>Likes</span>
-          <span className={styles.cardListNumber}>3000</span>
+          <span className={styles.cardListNumber}>{stats.likes}</span>
         </li>
       </ul>
     </div>
